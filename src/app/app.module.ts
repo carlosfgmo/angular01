1954f5364fa4d0dd8b51dcde42da0239b4b66db6
Carlos <carlosfgmo@gmail.com>
2020-05-15 07:47:22 -0500

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { routing, appRoutingProviders } from './app.routing';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { MiComponente } from './components/mi-componente/mi-componente.component';
 import { PeliculasComponent } from './components/peliculas/peliculas.component';
---
+++ src/app/services/article.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from  '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Article } from '../models/article';
+
+@Injectable()
+export class ArticleService {
+
+    constructor(
+        
+    ) {}
+
+}
